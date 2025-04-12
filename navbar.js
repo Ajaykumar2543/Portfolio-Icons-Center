ff0616f80f186612f1a6de4924cdf780a6569a96
Elaboina Ajaykumar <119422076+Ajaykumar2543@users.noreply.github.com>
2025-04-12 11:41:00 +0530

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinksContainer = document.querySelector('.nav-link-container');
+    const menuHamburger = document.querySelector('.menu-hamburger');
+    const blurContent = document.querySelector('.blur-content');
+    const barMenus = document.querySelectorAll('.bar-menu');
+    const resText = document.querySelector('.res-text');
+    const dropdowns = document.querySelectorAll('.dropdown');
+
+    menuHamburger.addEventListener('click', () => {
+        navLinksContainer.classList.toggle('active');
+        menuHamburger.classList.toggle('active');
+        toggleBlur();
+        toggleResTextVisibility();
+    });
+
+    const navLinks = document.querySelectorAll('.nav-link-container a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navLinksContainer.classList.remove('active');
+            menuHamburger.classList.remove('active');
+            blurContent.style.display = 'none'; 
+        });
+    });
+});
